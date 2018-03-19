edf2b92a6d4fc90e0e6603be2e66db3c24c87323
Christopher Penrose <waffletower@users.noreply.github.com>
2018-03-18 18:55:32 -0700

    Added (system-options) macro and helper functions

--- src/sabisu/conf.clj
+++ src/sabisu/conf.clj
@@ -18,3 +18,43 @@
 
 (defn read-vals [m]
   (proc-vals read-string-not-symbols m))
+
+(defn merge-options [options-spec defaults input-options]
+  (let [mo (merge defaults (read-vals input-options))]
+    (if (s/valid? options-spec mo)
+      mo
+      (log/errorf
+       "merged options do not adhere to options-spec: %s"
+       (s/explain-str options-spec mo)))))
+
+(defn- create-options [options-spec defaults]
+  (merge-options options-spec defaults env))
+
+(defmacro system-options
+  "'system-options' is an initialization macro which generates specs for the options of a component system.
+    it requires the following arguments:
+  'system-name' a string or symbol used to create symbols for products of the macro (see below)
+  'coll' a collection of three value collections [unqualified-key validation-function default-value]
+  system-options will create the following:
+  a map containing all options keys and default values '{system-name}-defaults,'
+  individual specs for each option specified '{namespace-name}/{option-keyname}',
+  a hashmap spec containing all options '{system-name}-spec',
+  and a merged options map function '{system-name}-options' which is used to obtain system options
+  resulting from the merge of defaults and environment variables"
+  [system-name coll]
+  (let [ss (symbol (format "%s-spec" system-name))
+        ds (symbol (format "%s-defaults" system-name))
+        fs (symbol (format "%s-options" system-name))
+        [option-keys defaults option-specs]
+        (rotate
+         (map
+          (fn [[option-sym valid-fn default]]
+            (let [unqualified (keyword (name option-sym))
+                  qualified (keyword (str *ns*) (name option-sym))]
+              [qualified [unqualified default] `(s/def ~qualified ~valid-fn)]))
+          coll))]
+    `(do
+       ~@option-specs
+       (def ~ss (s/spec (s/keys :req-un [~@option-keys])))
+       (defn ~fs [] (create-options ~ss ~ds))
+       (def ~ds (hash-map ~@(flatten defaults))))))
