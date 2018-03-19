1029f2d0c4699229ac52021f87d30562089ca9ae
Christopher Penrose <waffletower@users.noreply.github.com>
2018-03-18 18:45:11 -0700

    initial checkin

---
+++ README.md
@@ -0,0 +1,18 @@
+# sabisu
+A Clojure micro-service utility library
+
+## Artifact
+
+via Clojars:
+`[waffletower/sabisu "0.1.0"]`
+
+## Tests
+
+`lein midje`
+
+## License
+
+Copyright © 2018 Christopher Penrose
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject waffletower/sabisu "0.1.0-SNAPSHOT"
+  :description "micro-service configuration library"
+  :url "https://github.com/waffletower/sabisu"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/spec.alpha "0.1.143"]
+                 [environ "1.1.0"]
+                 [com.taoensso/timbre "4.10.0"]
+                 [waffletower/serum "0.3.0-SNAPSHOT"]]
+  :deploy-repositories [["clojars-https" {:url "https://clojars.org/repo"
+                                          :username :env/clojars_user
+                                          :password :env/clojars_password}]]
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[midje "1.9.0"]]
+                   :plugins [[lein-midje "3.2.1"]]}})
