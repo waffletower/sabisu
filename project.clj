42ac9c78ee0fff08e0e234232d84e7dfd08aea64
Christopher Penrose <waffletower@users.noreply.github.com>
2021-01-07 14:36:32 -0800

    Updated clojure.spec.alpha dep

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/spec.alpha "0.2.187"]
+                 [org.clojure/spec.alpha "0.2.194"]
                  [org.clojure/tools.logging "1.1.0"]
                  [environ "1.1.0"]
                  [waffletower/serum "0.8.0"]]
